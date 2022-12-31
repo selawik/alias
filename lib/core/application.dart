fff94b9359cb33a5d53a0b92f33f21ca24928a4f
selawik <121557430+selawik@users.noreply.github.com>
2022-12-31 13:01:09 +0300

    Added common libraries

---
+++ lib/core/application.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Application extends StatelessWidget {
+  const Application({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Alias',
+      home: Container(),
+    );
+  }
+}
