4b01b786b3cb1095d8f1e59030965c3fad488c0a
selawik <121557430+selawik@users.noreply.github.com>
2023-01-01 14:53:34 +0300

    Added common file structure

---
+++ lib/core/injection.dart
@@ -0,0 +1,18 @@
+import 'package:get_it/get_it.dart';
+
+var locator = GetIt.instance;
+
+void initLocator() {
+  _initDataSources();
+  _initRepository();
+  _initUseCases();
+  _initBlocs();
+}
+
+void _initBlocs() {}
+
+void _initRepository() {}
+
+void _initUseCases() {}
+
+void _initDataSources() {}
