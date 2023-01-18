883720bfe5b6e1c42cd48e12af3af6b6de7c1f84
selawik <121557430+selawik@users.noreply.github.com>
2023-01-18 19:33:29 +0300

    Added Drift to project

---
+++ lib/core/database/database.dart
@@ -0,0 +1,29 @@
+// To open the database, add these imports to the existing file defining the
+// database class. They are used to open the database.
+import 'dart:io';
+
+import 'package:drift/drift.dart';
+import 'package:drift/native.dart';
+import 'package:path/path.dart' as p;
+import 'package:path_provider/path_provider.dart';
+
+@DriftDatabase(tables: [])
+class MyDatabase extends _$MyDatabase {
+  // we tell the database where to store the data with this constructor
+  MyDatabase() : super(_openConnection());
+
+  // you should bump this number whenever you change or add a table definition.
+  // Migrations are covered later in the documentation.
+  int get schemaVersion => 1;
+}
+
+LazyDatabase _openConnection() {
+  // the LazyDatabase util lets us find the right location for the file async.
+  return LazyDatabase(() async {
+    // put the database file, called db.sqlite here, into the documents folder
+    // for your app.
+    final dbFolder = await getApplicationDocumentsDirectory();
+    final file = File(p.join(dbFolder.path, 'db.sqlite'));
+    return NativeDatabase.createInBackground(file);
+  });
+}
