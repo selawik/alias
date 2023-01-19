e1cd56a28f168490c911b2468a91a10ffb0ac003
selawik <121557430+selawik@users.noreply.github.com>
2023-01-19 22:52:45 +0300

    Added di for category module

--- lib/core/injection.dart
+++ lib/core/injection.dart
@@ -1,4 +1,10 @@
 import 'package:alias/core/router/app_router.dart';
+import 'package:alias/feature/categories/data/data_source/category_data_source.dart';
+import 'package:alias/feature/categories/data/data_source/mock_category_data_source.dart';
+import 'package:alias/feature/categories/data/repository/category_repository_impl.dart';
+import 'package:alias/feature/categories/domain/repository/category_repository.dart';
+import 'package:alias/feature/categories/domain/usercases/load_categories.dart';
+import 'package:alias/feature/categories/presentation/bloc/categories_bloc.dart';
 import 'package:get_it/get_it.dart';
 
 var locator = GetIt.instance;
@@ -13,10 +19,21 @@ void initLocator() {
   locator.registerSingleton<AppRouter>(AppRouter());
 }
 
-void _initBlocs() {}
+void _initBlocs() {
+  locator.registerFactory<CategoriesBloc>(
+      () => CategoriesBloc(loadCategories: locator.get<LoadCategories>()));
+}
 
-void _initRepository() {}
+void _initRepository() {
+  locator.registerSingleton<CategoryRepository>(
+      CategoryRepositoryImpl(dataSource: locator.get<CategoryDataSource>()));
+}
 
-void _initUseCases() {}
+void _initUseCases() {
+  locator.registerFactory<LoadCategories>(
+      () => LoadCategories(repository: locator.get<CategoryRepository>()));
+}
 
-void _initDataSources() {}
+void _initDataSources() {
+  locator.registerSingleton<CategoryDataSource>(MockCategoryDataSource());
+}
