96504a47ecf48d40a6b6ecc2488228bf776c5d4e
selawik <121557430+selawik@users.noreply.github.com>
2023-01-20 18:05:51 +0300

    Added locator initialisation

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:alias/core/application.dart';
+import 'package:alias/core/injection.dart' as di;
 import 'package:flutter/material.dart';
 
 void main() {
+  di.initLocator();
   runApp(const Application());
 }
