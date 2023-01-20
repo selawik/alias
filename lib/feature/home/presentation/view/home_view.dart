387370a02d6cd2cb54af3900886587be4f54b16e
selawik <121557430+selawik@users.noreply.github.com>
2023-01-21 00:09:40 +0300

    Added home page

--- lib/feature/home/presentation/view/home_view.dart
+++ lib/feature/home/presentation/view/home_view.dart
@@ -1,3 +1,5 @@
+import 'package:alias/core/injection.dart' as di;
+import 'package:alias/core/router/app_router.dart';
 import 'package:flutter/material.dart';
 
 class HomeView extends StatelessWidget {
@@ -5,6 +7,26 @@ class HomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    var router = di.locator.get<AppRouter>();
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 32),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          ElevatedButton(
+            onPressed: () => router.push(const CategoryPageRoute()),
+            child: const Text('Новая игра'),
+          ),
+          const SizedBox(height: 12),
+          ElevatedButton(
+            onPressed: () => router.push(const SettingsPageRoute()),
+            child: const Text('Настройки'),
+          ),
+          SizedBox(height: MediaQuery.of(context).padding.bottom + 16),
+        ],
+      ),
+    );
   }
 }
