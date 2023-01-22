7c908cd374c7df6be216d3c42ff60d01a55ce50e
selawik <121557430+selawik@users.noreply.github.com>
2023-01-22 15:54:10 +0300

    Added command view

--- lib/feature/commands/presentation/view/commands_view.dart
+++ lib/feature/commands/presentation/view/commands_view.dart
@@ -1,10 +1,186 @@
+import 'package:alias/feature/commands/data/models/command.dart';
+import 'package:alias/feature/commands/presentation/bloc/commands_bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class CommandsView extends StatelessWidget {
-  const CommandsView({Key? key}) : super(key: key);
+  final ScrollController _scrollController = ScrollController();
+
+  CommandsView({
+    Key? key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Stack(
+      children: [
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            const SizedBox(height: 16),
+            Expanded(
+              child: _buildList(context),
+            ),
+            const SizedBox(height: 16),
+            _buildAddButton(context),
+            const SizedBox(height: 16),
+            _buildContinueButton(context),
+          ],
+        ),
+      ],
+    );
   }
+
+  Widget _buildList(BuildContext context) {
+    return BlocBuilder<CommandsBloc, CommandsState>(
+      builder: (context, state) {
+        return state.when(
+          initial: () => Container(),
+          loading: () => const CircularProgressIndicator(),
+          loaded: (allCommands, addedCommands) =>
+              _buildCommandsList(context, addedCommands, addedCommands),
+        );
+      },
+    );
+  }
+
+  Widget _buildCommandsList(
+    BuildContext context,
+    List<Command> allCommands,
+    List<Command> addedCommands,
+  ) {
+    return ListView.separated(
+      shrinkWrap: true,
+      controller: _scrollController,
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      itemBuilder: (context, index) =>
+          _buildListItem(context, addedCommands[index]),
+      separatorBuilder: (context, index) => const SizedBox(height: 8),
+      itemCount: addedCommands.length,
+    );
+  }
+
+  Widget _buildListItem(BuildContext context, Command item) {
+    var bloc = BlocProvider.of<CommandsBloc>(context);
+    var currentState = bloc.state;
+    var textStyle = Theme.of(context).textTheme.headline2;
+
+    var listItem = Container(
+      alignment: Alignment.centerLeft,
+      padding: const EdgeInsets.all(16),
+      decoration: BoxDecoration(
+          //color: AppColors.lavender,
+          //borderRadius: ThemeBuilder.defaultBorderRadius,
+          ),
+      child: Row(
+        children: [
+          Expanded(
+            child: Text(
+              item.name,
+              //style: textStyle?.copyWith(color: AppColors.black),
+            ),
+          ),
+          const SizedBox(width: 8),
+          currentState.whenOrNull(loaded: (allCommands, addedCommands) {
+                if (addedCommands.length > 1) {
+                  return GestureDetector(
+                    onTap: () => bloc.add(
+                      CommandsEvent.removeCommand(command: item),
+                    ),
+                    child: const Icon(Icons.remove),
+                  );
+                }
+              }) ??
+              Container(),
+        ],
+      ),
+    );
+
+    return listItem;
+  }
+
+  Widget _wrapItemIntoDismissible(
+    BuildContext context, {
+    required Widget child,
+    required void Function() callback,
+    required Command item,
+  }) {
+    return Dismissible(
+      onDismissed: (direction) => callback(),
+      key: UniqueKey(),
+      child: child,
+    );
+  }
+
+  Widget _buildAddButton(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 16, right: 16),
+      child: ElevatedButton(
+        onPressed: () {
+          //HapticFeedback.mediumImpact();
+          _onAddPressed(context);
+        },
+        //style: ThemeBuilder.orangeButtonStyle,
+        child: const Icon(
+          Icons.add,
+          size: 40,
+        ),
+      ),
+    );
+  }
+
+  Widget _buildContinueButton(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(
+        bottom: MediaQuery.of(context).padding.bottom + 16,
+        left: 16,
+        right: 16,
+      ),
+      child: ElevatedButton(
+        onPressed: () => _onContinuePressed(context),
+        child: const Text('Продолжить'),
+      ),
+    );
+  }
+
+  void _onContinuePressed(BuildContext context) {
+    var commandsBloc = BlocProvider.of<CommandsBloc>(context);
+    //var aliasBloc = BlocProvider.of<AliasBloc>(context);
+    var state = commandsBloc.state;
+
+    // HapticFeedback.mediumImpact();
+    //
+    // if (state is AddCommands) {
+    //   aliasBloc.add(CommandsFormedEvent(commands: state.addedCommands));
+    // }
+  }
+
+  void _onAddPressed(BuildContext context) {
+    var bloc = BlocProvider.of<CommandsBloc>(context);
+
+    bloc.add(const CommandsEvent.addCommand());
+
+    Future.delayed(
+      const Duration(microseconds: 300),
+      () => _scrollController.animateTo(
+        _scrollController.position.maxScrollExtent,
+        duration: const Duration(milliseconds: 300),
+        curve: Curves.easeInOut,
+      ),
+    );
+  }
+
+  // void _onBlockListenerTrigger(BuildContext context, AliasState state) {
+  //   var commandsBloc = BlocProvider.of<CommandsBloc>(context);
+  //   var commandsState = commandsBloc.state;
+  //
+  //   if (state is CommandStep && commandsState is AddCommands) {
+  //     di.locator<AppRouter>().replace(const GamePageRoute());
+  //   }
+  //
+  //   if (state is WordsLoadingError) {
+  //     di.locator<AppRouter>().pop();
+  //     showMessage(context, state.failure.message);
+  //   }
+  // }
 }
