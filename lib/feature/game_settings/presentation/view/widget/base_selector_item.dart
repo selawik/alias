bca3a75e7fbee7b865fbcc3718f07b07db323786
selawik <121557430+selawik@users.noreply.github.com>
2023-01-23 22:31:18 +0300

    Create base selector item

---
+++ lib/feature/game_settings/presentation/view/widget/base_selector_item.dart
@@ -0,0 +1,47 @@
+import 'package:alias/core/constants/app_colors.dart';
+import 'package:alias/core/theme/theme_builder.dart';
+import 'package:flutter/material.dart';
+
+class BaseSelectorItem extends StatelessWidget {
+  final String title;
+  final void Function() onTap;
+  final bool isSelected;
+
+  const BaseSelectorItem({
+    Key? key,
+    required this.onTap,
+    required this.title,
+    required this.isSelected,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => onTap(),
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        decoration: BoxDecoration(
+          borderRadius: ThemeBuilder.defaultBorderRadius,
+          border: isSelected
+              ? Border.all(color: AppColors.appBackgroundDark)
+              : null,
+          color: isSelected ? AppColors.buttonColor : AppColors.white,
+          boxShadow: [
+            BoxShadow(
+              offset: const Offset(2, 3),
+              blurRadius: 5,
+              color: AppColors.black.withOpacity(0.4),
+            ),
+          ],
+        ),
+        child: Text(
+          title,
+          style: Theme.of(context).textTheme.button?.copyWith(
+                color: isSelected ? null : AppColors.black,
+                fontWeight: FontWeight.w500,
+              ),
+        ),
+      ),
+    );
+  }
+}
--- lib/feature/game_settings/presentation/view/widget/command_move_time_selector.dart
+++ lib/feature/game_settings/presentation/view/widget/command_move_time_selector.dart
@@ -1,6 +1,5 @@
-import 'package:alias/core/constants/app_colors.dart';
-import 'package:alias/core/theme/theme_builder.dart';
 import 'package:alias/feature/game_settings/domain/model/command_move_time_enum.dart';
+import 'package:alias/feature/game_settings/presentation/view/widget/base_selector_item.dart';
 import 'package:flutter/material.dart';
 
 class CommandMoveTimeSelector extends StatelessWidget {
@@ -25,57 +24,13 @@ class CommandMoveTimeSelector extends StatelessWidget {
       spacing: 12,
       children: CommandMoveTimeEnum.values
           .map(
-            (element) => CommandMoveItem(
-              type: element,
+            (element) => BaseSelectorItem(
+              onTap: () {},
               isSelected: element == selectedItem,
-              onTap: (moveTime) {},
+              title: element.toString(),
             ),
           )
           .toList(),
-    );
-  }
-}
-
-class CommandMoveItem extends StatelessWidget {
-  final CommandMoveTimeEnum type;
-  final void Function(CommandMoveTimeEnum) onTap;
-  final bool isSelected;
-
-  const CommandMoveItem({
-    Key? key,
-    required this.type,
-    required this.onTap,
-    required this.isSelected,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () => onTap(type),
-      child: Container(
-        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-        decoration: BoxDecoration(
-          borderRadius: ThemeBuilder.defaultBorderRadius,
-          border: isSelected
-              ? Border.all(color: AppColors.appBackgroundDark)
-              : null,
-          color: isSelected ? AppColors.buttonColor : AppColors.white,
-          boxShadow: [
-            BoxShadow(
-              offset: const Offset(2, 3),
-              blurRadius: 5,
-              color: AppColors.black.withOpacity(0.4),
-            ),
-          ],
-        ),
-        child: Text(
-          type.toString(),
-          style: Theme.of(context).textTheme.button?.copyWith(
-                color: isSelected ? null : AppColors.black,
-                fontWeight: FontWeight.w500,
-              ),
-        ),
-      ),
     );
   }
 }
