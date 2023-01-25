0ceca8fea47e1ed622ea466406cef44af33cd065
selawik <121557430+selawik@users.noreply.github.com>
2023-01-25 22:15:09 +0300

    Add game settings bloc event handling

--- lib/core/injection.config.dart
+++ lib/core/injection.config.dart
@@ -15,9 +15,9 @@ import 'package:alias/feature/categories/data/repository/firebase_category_repos
 import 'package:alias/feature/categories/domain/repository/category_repository.dart'
     as _i6;
 import 'package:alias/feature/categories/domain/usercases/load_categories.dart'
-    as _i12;
+    as _i13;
 import 'package:alias/feature/categories/presentation/bloc/categories_bloc.dart'
-    as _i14;
+    as _i15;
 import 'package:alias/feature/commands/data/data_source/commands_data_source.dart'
     as _i8;
 import 'package:alias/feature/commands/data/data_source/firebase_commands_data_source.dart'
@@ -27,9 +27,11 @@ import 'package:alias/feature/commands/data/repository/commands_repository_impl.
 import 'package:alias/feature/commands/domain/repository/commands_repository.dart'
     as _i10;
 import 'package:alias/feature/commands/domain/usercases/load_commands.dart'
-    as _i13;
+    as _i14;
 import 'package:alias/feature/commands/presentation/bloc/commands_bloc.dart'
-    as _i15;
+    as _i16;
+import 'package:alias/feature/game_settings/presentation/bloc/game_settings_bloc.dart'
+    as _i12;
 import 'package:get_it/get_it.dart' as _i1;
 import 'package:injectable/injectable.dart' as _i2;
 
@@ -53,14 +55,15 @@ extension GetItInjectableX on _i1.GetIt {
     gh.factory<_i8.CommandsDataSource>(() => _i9.FirebaseCommandsDataSource());
     gh.factory<_i10.CommandsRepository>(() =>
         _i11.CommandsRepositoryImpl(dataSource: gh<_i8.CommandsDataSource>()));
-    gh.factory<_i12.LoadCategories>(
-        () => _i12.LoadCategories(repository: gh<_i6.CategoryRepository>()));
-    gh.factory<_i13.LoadCommands>(
-        () => _i13.LoadCommands(repository: gh<_i10.CommandsRepository>()));
-    gh.factory<_i14.CategoriesBloc>(
-        () => _i14.CategoriesBloc(loadCategories: gh<_i12.LoadCategories>()));
-    gh.factory<_i15.CommandsBloc>(
-        () => _i15.CommandsBloc(gh<_i13.LoadCommands>()));
+    gh.factory<_i12.GameSettingsBloc>(() => _i12.GameSettingsBloc());
+    gh.factory<_i13.LoadCategories>(
+        () => _i13.LoadCategories(repository: gh<_i6.CategoryRepository>()));
+    gh.factory<_i14.LoadCommands>(
+        () => _i14.LoadCommands(repository: gh<_i10.CommandsRepository>()));
+    gh.factory<_i15.CategoriesBloc>(
+        () => _i15.CategoriesBloc(loadCategories: gh<_i13.LoadCategories>()));
+    gh.factory<_i16.CommandsBloc>(
+        () => _i16.CommandsBloc(gh<_i14.LoadCommands>()));
     return this;
   }
 }
--- lib/feature/game_settings/presentation/bloc/game_settings_bloc.dart
+++ lib/feature/game_settings/presentation/bloc/game_settings_bloc.dart
@@ -1,17 +1,18 @@
 import 'package:alias/feature/game_settings/domain/model/binary_selector_type.dart';
 import 'package:alias/feature/game_settings/domain/model/command_move_selector_type.dart';
-import 'package:alias/feature/game_settings/presentation/view/widget/command_move_time_selector.dart';
 import 'package:bloc/bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:injectable/injectable.dart';
 
 part 'game_settings_bloc.freezed.dart';
 part 'game_settings_event.dart';
 part 'game_settings_state.dart';
 
+@Injectable()
 class GameSettingsBloc extends Bloc<GameSettingsEvent, GameSettingsState> {
   GameSettingsBloc()
       : super(const GameSettingsState.ready(
-          CommandMoveModeSelector.minute,
+          CommandMoveMode.minute,
           BinarySelectorMode.enabled,
           BinarySelectorMode.disabled,
         )) {
@@ -20,9 +21,27 @@ class GameSettingsBloc extends Bloc<GameSettingsEvent, GameSettingsState> {
     on<_MoveTimeChanged>(_onMoveTimeChanged);
   }
 
-  void _onPenaltyModeChanged(_PenaltyModeChanged event, Emitter emit) {}
+  void _onPenaltyModeChanged(_PenaltyModeChanged event, Emitter emit) {
+    emit(GameSettingsState.ready(
+      state.time,
+      state.lastWordMode,
+      event.mode,
+    ));
+  }
 
-  void _onLastWordModeChanged(_LastWordModeChanged event, Emitter emit) {}
+  void _onLastWordModeChanged(_LastWordModeChanged event, Emitter emit) {
+    emit(GameSettingsState.ready(
+      state.time,
+      event.mode,
+      state.penaltyMode,
+    ));
+  }
 
-  void _onMoveTimeChanged(_MoveTimeChanged event, Emitter emit) {}
+  void _onMoveTimeChanged(_MoveTimeChanged event, Emitter emit) {
+    emit(GameSettingsState.ready(
+      event.moveTime,
+      state.lastWordMode,
+      state.penaltyMode,
+    ));
+  }
 }
