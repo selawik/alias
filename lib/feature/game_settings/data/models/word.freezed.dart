3bab2049f5cf860e8c19cf7093d5502aaed23382
selawik <121557430+selawik@users.noreply.github.com>
2023-01-26 22:11:46 +0300

    Add word model

---
+++ lib/feature/game_settings/data/models/word.freezed.dart
@@ -0,0 +1,179 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target
+
+part of 'word.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+Word _$WordFromJson(Map<String, dynamic> json) {
+  return _Word.fromJson(json);
+}
+
+/// @nodoc
+mixin _$Word {
+  int get wordId => throw _privateConstructorUsedError;
+  String get name => throw _privateConstructorUsedError;
+  int get categoryId => throw _privateConstructorUsedError;
+
+  Map<String, dynamic> toJson() => throw _privateConstructorUsedError;
+  @JsonKey(ignore: true)
+  $WordCopyWith<Word> get copyWith => throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $WordCopyWith<$Res> {
+  factory $WordCopyWith(Word value, $Res Function(Word) then) =
+      _$WordCopyWithImpl<$Res, Word>;
+  @useResult
+  $Res call({int wordId, String name, int categoryId});
+}
+
+/// @nodoc
+class _$WordCopyWithImpl<$Res, $Val extends Word>
+    implements $WordCopyWith<$Res> {
+  _$WordCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? wordId = null,
+    Object? name = null,
+    Object? categoryId = null,
+  }) {
+    return _then(_value.copyWith(
+      wordId: null == wordId
+          ? _value.wordId
+          : wordId // ignore: cast_nullable_to_non_nullable
+              as int,
+      name: null == name
+          ? _value.name
+          : name // ignore: cast_nullable_to_non_nullable
+              as String,
+      categoryId: null == categoryId
+          ? _value.categoryId
+          : categoryId // ignore: cast_nullable_to_non_nullable
+              as int,
+    ) as $Val);
+  }
+}
+
+/// @nodoc
+abstract class _$$_WordCopyWith<$Res> implements $WordCopyWith<$Res> {
+  factory _$$_WordCopyWith(_$_Word value, $Res Function(_$_Word) then) =
+      __$$_WordCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call({int wordId, String name, int categoryId});
+}
+
+/// @nodoc
+class __$$_WordCopyWithImpl<$Res> extends _$WordCopyWithImpl<$Res, _$_Word>
+    implements _$$_WordCopyWith<$Res> {
+  __$$_WordCopyWithImpl(_$_Word _value, $Res Function(_$_Word) _then)
+      : super(_value, _then);
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? wordId = null,
+    Object? name = null,
+    Object? categoryId = null,
+  }) {
+    return _then(_$_Word(
+      wordId: null == wordId
+          ? _value.wordId
+          : wordId // ignore: cast_nullable_to_non_nullable
+              as int,
+      name: null == name
+          ? _value.name
+          : name // ignore: cast_nullable_to_non_nullable
+              as String,
+      categoryId: null == categoryId
+          ? _value.categoryId
+          : categoryId // ignore: cast_nullable_to_non_nullable
+              as int,
+    ));
+  }
+}
+
+/// @nodoc
+@JsonSerializable()
+class _$_Word implements _Word {
+  const _$_Word(
+      {required this.wordId, required this.name, required this.categoryId});
+
+  factory _$_Word.fromJson(Map<String, dynamic> json) => _$$_WordFromJson(json);
+
+  @override
+  final int wordId;
+  @override
+  final String name;
+  @override
+  final int categoryId;
+
+  @override
+  String toString() {
+    return 'Word(wordId: $wordId, name: $name, categoryId: $categoryId)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$_Word &&
+            (identical(other.wordId, wordId) || other.wordId == wordId) &&
+            (identical(other.name, name) || other.name == name) &&
+            (identical(other.categoryId, categoryId) ||
+                other.categoryId == categoryId));
+  }
+
+  @JsonKey(ignore: true)
+  @override
+  int get hashCode => Object.hash(runtimeType, wordId, name, categoryId);
+
+  @JsonKey(ignore: true)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$_WordCopyWith<_$_Word> get copyWith =>
+      __$$_WordCopyWithImpl<_$_Word>(this, _$identity);
+
+  @override
+  Map<String, dynamic> toJson() {
+    return _$$_WordToJson(
+      this,
+    );
+  }
+}
+
+abstract class _Word implements Word {
+  const factory _Word(
+      {required final int wordId,
+      required final String name,
+      required final int categoryId}) = _$_Word;
+
+  factory _Word.fromJson(Map<String, dynamic> json) = _$_Word.fromJson;
+
+  @override
+  int get wordId;
+  @override
+  String get name;
+  @override
+  int get categoryId;
+  @override
+  @JsonKey(ignore: true)
+  _$$_WordCopyWith<_$_Word> get copyWith => throw _privateConstructorUsedError;
+}
---
+++ lib/feature/game_settings/data/models/word.g.dart
@@ -0,0 +1,19 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'word.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$_Word _$$_WordFromJson(Map<String, dynamic> json) => _$_Word(
+      wordId: json['wordId'] as int,
+      name: json['name'] as String,
+      categoryId: json['categoryId'] as int,
+    );
+
+Map<String, dynamic> _$$_WordToJson(_$_Word instance) => <String, dynamic>{
+      'wordId': instance.wordId,
+      'name': instance.name,
+      'categoryId': instance.categoryId,
+    };
