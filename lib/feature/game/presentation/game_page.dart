759da33ccccc1c769370d718d70e92424a078884
selawik <121557430+selawik@users.noreply.github.com>
2023-01-26 22:16:25 +0300

    Remove bloc provider

--- lib/feature/game/presentation/game_page.dart
+++ lib/feature/game/presentation/game_page.dart
@@ -1,23 +1,17 @@
-import 'package:alias/core/injection.dart' as di;
 import 'package:alias/core/widget/custom_app_bar.dart';
-import 'package:alias/feature/game/presentation/bloc/game_bloc.dart';
 import 'package:alias/feature/game/presentation/view/game_view.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
 class GamePage extends StatelessWidget {
   const GamePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: const CustomAppBar(
+    return const Scaffold(
+      appBar: CustomAppBar(
         title: '123',
       ),
-      body: BlocProvider(
-        create: (context) => di.locator.get<GameBloc>(),
-        child: const GameView(),
-      ),
+      body: GameView(),
     );
   }
 }
