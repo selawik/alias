37ca265b981542e987afd023306840eec15ee101
selawik <121557430+selawik@users.noreply.github.com>
2023-01-26 22:12:34 +0300

    Move GameBloc provider to main

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:alias/core/application.dart';
 import 'package:alias/core/injection.dart' as di;
+import 'package:alias/feature/game/presentation/bloc/game_bloc.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'firebase_options.dart';
 
@@ -14,5 +16,12 @@ void main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
-  runApp(const Application());
+  runApp(
+    MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (context) => di.locator.get<GameBloc>()),
+      ],
+      child: const Application(),
+    ),
+  );
 }
