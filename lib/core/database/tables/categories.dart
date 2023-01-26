0eb8ab4cae457f96ebcf65b3b0051807193164e0
selawik <121557430+selawik@users.noreply.github.com>
2023-01-26 22:11:28 +0300

    Rename category table

--- lib/core/database/tables/categories.dart
+++ lib/core/database/tables/categories.dart
@@ -1,7 +1,7 @@
 import 'package:drift/drift.dart';
 
 @DataClassName('Category')
-class Categories extends Table {
+class CategoriesTable extends Table {
   IntColumn get id => integer().autoIncrement()();
   TextColumn get description => text()();
 }
