6a7a62129e06e828ef0a4b819299c2ee9dea0c21
selawik <121557430+selawik@users.noreply.github.com>
2023-02-05 18:47:18 +0300

    Remove autoincrement field

--- lib/core/database/tables/played_word.dart
+++ lib/core/database/tables/played_word.dart
@@ -2,7 +2,7 @@ import 'package:drift/drift.dart';
 
 @DataClassName('PlayedWords')
 class PlayedWord extends Table {
-  IntColumn get playedWordId => integer().autoIncrement()();
+  IntColumn get playedWordId => integer().unique()();
 
   TextColumn get name => text()();
 }
