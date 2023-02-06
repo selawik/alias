fc47ac3dcb14f226da1baa169b4db6401287829f
selawik <121557430+selawik@users.noreply.github.com>
2023-02-06 22:09:51 +0300

    Customize snackbar

--- lib/core/mixin/snackbar_mixin.dart
+++ lib/core/mixin/snackbar_mixin.dart
@@ -1,8 +1,13 @@
+import 'package:alias/core/theme/theme_builder.dart';
 import 'package:flutter/material.dart';
 
-mixin ShowMessageMixin on StatelessWidget {
+mixin SnackbarMixin on StatelessWidget {
   void showMessage(BuildContext context, String message) {
     var snackBar = SnackBar(
+      shape: RoundedRectangleBorder(
+        borderRadius: ThemeBuilder.defaultBorderRadius,
+      ),
+      behavior: SnackBarBehavior.floating,
       content: Text(message),
     );
 
