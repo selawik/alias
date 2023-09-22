c4596d3e3ee1832e323e1b662e91faeede73809a
selawik <121557430+selawik@users.noreply.github.com>
2023-09-22 19:14:09 +0300

    Add show error method in stackbar mixin

--- lib/src/core/mixin/snackbar_mixin.dart
+++ lib/src/core/mixin/snackbar_mixin.dart
@@ -1,9 +1,23 @@
+import 'package:alias/src/core/constants/app_colors.dart';
 import 'package:alias/src/core/theme/theme_builder.dart';
 import 'package:flutter/material.dart';
 
 mixin SnackbarMixin on StatelessWidget {
   void showMessage(BuildContext context, String message) {
     final snackBar = SnackBar(
+      shape: RoundedRectangleBorder(
+        borderRadius: ThemeBuilder.defaultBorderRadius,
+      ),
+      behavior: SnackBarBehavior.floating,
+      content: Text(message),
+    );
+
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  }
+
+  void showError(BuildContext context, String message) {
+    final snackBar = SnackBar(
+      backgroundColor: AppColors.red,
       shape: RoundedRectangleBorder(
         borderRadius: ThemeBuilder.defaultBorderRadius,
       ),
